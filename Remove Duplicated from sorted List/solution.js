51ff9d867f2975e18d304ee73ffd8ced500e0891
Snehal Mastud <smastud0@gmail.com>
2026-03-19 11:52:33 +0530

    Create solution.js

---
+++ Remove Duplicated from sorted List/solution.js
@@ -0,0 +1,11 @@
+var deleteDuplicates = function (head) {
+    let cur = head;
+    while (cur && cur.next) {
+        if (cur.next.val === cur.val) {
+            cur.next = cur.next.next;
+        } else {
+            cur = cur.next;
+        }
+    }
+    return head;
+};
