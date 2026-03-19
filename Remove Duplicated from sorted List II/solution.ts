6da6f5745562e85edee8a0cef6f4f16798637fe6
Snehal Mastud <smastud0@gmail.com>
2026-03-19 11:51:45 +0530

    Create solution.ts

---
+++ Remove Duplicated from sorted List II/solution.ts
@@ -0,0 +1,17 @@
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    const dummy = new ListNode(0, head);
+    let pre = dummy;
+    let cur = head;
+    while (cur) {
+        while (cur.next && cur.val === cur.next.val) {
+            cur = cur.next;
+        }
+        if (pre.next === cur) {
+            pre = cur;
+        } else {
+            pre.next = cur.next;
+        }
+        cur = cur.next;
+    }
+    return dummy.next;
+}
