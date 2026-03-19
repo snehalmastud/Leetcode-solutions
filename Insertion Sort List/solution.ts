e66ffd30d0606ddee9cb5520a19f15aeff43eadd
Snehal Mastud <smastud0@gmail.com>
2026-03-19 13:41:29 +0530

    Create solution.ts

---
+++ Insertion Sort List/solution.ts
@@ -0,0 +1,23 @@
+var insertionSortList = function (head) {
+    if (head == null || head.next == null) return head;
+    let dummy = new ListNode(head.val, head);
+    let prev = dummy,
+        cur = head;
+    while (cur != null) {
+        if (prev.val <= cur.val) {
+            prev = cur;
+            cur = cur.next;
+            continue;
+        }
+        let p = dummy;
+        while (p.next.val <= cur.val) {
+            p = p.next;
+        }
+        let t = cur.next;
+        cur.next = p.next;
+        p.next = cur;
+        prev.next = t;
+        cur = t;
+    }
+    return dummy.next;
+};
