0f20a3af02cfb389a781167819475969e300623f
Snehal Mastud <smastud0@gmail.com>
2026-03-19 14:24:54 +0530

    Create solution.ts

---
+++ Remove Linked List Elements/solution.ts
@@ -0,0 +1,12 @@
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+    const dummy: ListNode = new ListNode(0, head);
+    let cur: ListNode = dummy;
+    while (cur.next != null) {
+        if (cur.next.val === val) {
+            cur.next = cur.next.next;
+        } else {
+            cur = cur.next;
+        }
+    }
+    return dummy.next;
+}
