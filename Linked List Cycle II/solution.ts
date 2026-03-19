89c41b61dae304bee97272df8d9f34cecb5f57bb
Snehal Mastud <smastud0@gmail.com>
2026-03-19 13:31:29 +0530

    Create solution.ts

---
+++ Linked List Cycle II/solution.ts
@@ -0,0 +1,16 @@
+function detectCycle(head: ListNode | null): ListNode | null {
+    let [slow, fast] = [head, head];
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            let ans = head;
+            while (ans !== slow) {
+                ans = ans.next;
+                slow = slow.next;
+            }
+            return ans;
+        }
+    }
+    return null;
+}
