c3b4b7ea6156bddc0378f24fcc4c7d40f1f61453
Snehal Mastud <smastud0@gmail.com>
2026-03-19 11:15:54 +0530

    Create solution.js

---
+++ Next Permutation/solution.js
@@ -0,0 +1,17 @@
+var nextPermutation = function (nums) {
+    const n = nums.length;
+    let i = n - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1]) {
+        --i;
+    }
+    if (i >= 0) {
+        let j = n - 1;
+        while (j > i && nums[j] <= nums[i]) {
+            --j;
+        }
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+    for (i = i + 1, j = n - 1; i < j; ++i, --j) {
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+};
