f864285ffc847d69559cb1e4ac8bff33bb8c0b78
Snehal Mastud <smastud0@gmail.com>
2026-03-19 10:42:29 +0530

    Create solution.js

---
+++ Two Sum/solution.js
@@ -0,0 +1,11 @@
+var twoSum = function (nums, target) {
+    const m = new Map();
+    for (let i = 0; ; ++i) {
+        const x = nums[i];
+        const y = target - x;
+        if (m.has(y)) {
+            return [m.get(y), i];
+        }
+        m.set(x, i);
+    }
+};
