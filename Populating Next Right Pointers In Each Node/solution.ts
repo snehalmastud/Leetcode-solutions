4bba0d77bb2d946debdad8ebfcccf404e90c1fd2
Snehal Mastud <smastud0@gmail.com>
2026-03-19 12:34:43 +0530

    Create solution.ts

---
+++ Populating Next Right Pointers In Each Node/solution.ts
@@ -0,0 +1,13 @@
+function connect(root: Node | null): Node | null {
+    if (root == null || root.left == null) {
+        return root;
+    }
+    const { left, right, next } = root;
+    left.next = right;
+    if (next != null) {
+        right.next = next.left;
+    }
+    connect(left);
+    connect(right);
+    return root;
+}
