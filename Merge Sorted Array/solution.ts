298b5757716121b6cf25aa70958068fcd05b35de
Snehal Mastud <smastud0@gmail.com>
2026-03-19 11:55:56 +0530

    Create solution.ts

---
+++ Merge Sorted Array/solution.ts
@@ -0,0 +1,5 @@
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+    for (let i = m - 1, j = n - 1, k = m + n - 1; j >= 0; --k) {
+        nums1[k] = i >= 0 && nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+    }
+}
