c82c0686f2c8debd02e438c486617a7bc227bc81
Snehal Mastud <smastud0@gmail.com>
2026-03-19 16:45:15 +0530

    Create solution.js

---
+++ Wiggle Sort II/solution.js
@@ -0,0 +1,22 @@
+var wiggleSort = function (nums) {
+    let bucket = new Array(5001).fill(0);
+    for (const v of nums) {
+        bucket[v]++;
+    }
+    const n = nums.length;
+    let j = 5000;
+    for (let i = 1; i < n; i += 2) {
+        while (bucket[j] == 0) {
+            --j;
+        }
+        nums[i] = j;
+        --bucket[j];
+    }
+    for (let i = 0; i < n; i += 2) {
+        while (bucket[j] == 0) {
+            --j;
+        }
+        nums[i] = j;
+        --bucket[j];
+    }
+};
