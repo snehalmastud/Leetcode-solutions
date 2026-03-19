f07627000690d0bed17b28c6df64f4d3d30a8fd2
Snehal Mastud <smastud0@gmail.com>
2026-03-19 11:46:32 +0530

    Create solution.ts

---
+++ Sort Colors/solution.ts
@@ -0,0 +1,17 @@
+function sortColors(nums: number[]): void {
+    let i = -1;
+    let j = nums.length;
+    let k = 0;
+    while (k < j) {
+        if (nums[k] === 0) {
+            ++i;
+            [nums[i], nums[k]] = [nums[k], nums[i]];
+            ++k;
+        } else if (nums[k] === 2) {
+            --j;
+            [nums[j], nums[k]] = [nums[k], nums[j]];
+        } else {
+            ++k;
+        }
+    }
+}
