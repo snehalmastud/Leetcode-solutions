c982433b74372af87bddb04eb4008d94abb25792
Snehal Mastud <smastud0@gmail.com>
2026-03-19 13:48:26 +0530

    Create solution.java

---
+++ Binary Tree Upside Down/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if (root == null || root.left == null) {
+            return root;
+        }
+        TreeNode newRoot = upsideDownBinaryTree(root.left);
+        root.left.right = root;
+        root.left.left = root.right;
+        root.left = null;
+        root.right = null;
+        return newRoot;
+    }
+}
