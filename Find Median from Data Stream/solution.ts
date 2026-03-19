a7ce9dd4224237b1a04413d0ed7a3273a316166e
Snehal Mastud <smastud0@gmail.com>
2026-03-19 15:54:36 +0530

    Create solution.ts

---
+++ Find Median from Data Stream/solution.ts
@@ -0,0 +1,31 @@
+class MedianFinder {
+    private nums: number[];
+
+    constructor() {
+        this.nums = [];
+    }
+
+    addNum(num: number): void {
+        const { nums } = this;
+        let l = 0;
+        let r = nums.length;
+        while (l < r) {
+            const mid = (l + r) >>> 1;
+            if (nums[mid] < num) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        nums.splice(l, 0, num);
+    }
+
+    findMedian(): number {
+        const { nums } = this;
+        const n = nums.length;
+        if ((n & 1) === 1) {
+            return nums[n >> 1];
+        }
+        return (nums[n >> 1] + nums[(n >> 1) - 1]) / 2;
+    }
+}
