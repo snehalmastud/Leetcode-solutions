7382dbf8d1d0f57975561f778c1a30d845f29fa8
Snehal Mastud <smastud0@gmail.com>
2026-03-19 11:33:04 +0530

    Create solution.js

---
+++ Jump Game/solution.js
@@ -0,0 +1,10 @@
+var canJump = function (nums) {
+    let mx = 0;
+    for (let i = 0; i < nums.length; ++i) {
+        if (mx < i) {
+            return false;
+        }
+        mx = Math.max(mx, i + nums[i]);
+    }
+    return true;
+};
