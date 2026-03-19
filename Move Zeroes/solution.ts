2d830068e759c0793312449f757422b6e7a09568
Snehal Mastud <smastud0@gmail.com>
2026-03-19 15:45:59 +0530

    Create solution.ts

---
+++ Move Zeroes/solution.ts
@@ -0,0 +1,12 @@
+function moveZeroes(nums: number[]): void {
+    const n = nums.length;
+    let i = 0;
+    for (let j = 0; j < n; j++) {
+        if (nums[j]) {
+            if (j > i) {
+                [nums[i], nums[j]] = [nums[j], 0];
+            }
+            i++;
+        }
+    }
+}
