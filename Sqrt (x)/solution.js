85c6fc5d4a78b5a67408692baf63aa8c8069e6ff
Snehal Mastud <smastud0@gmail.com>
2026-03-19 11:42:29 +0530

    Create solution.js

---
+++ Sqrt (x)/solution.js
@@ -0,0 +1,12 @@
+var mySqrt = function (x) {
+    let [l, r] = [0, x];
+    while (l < r) {
+        const mid = (l + r + 1) >> 1;
+        if (mid > x / mid) {
+            r = mid - 1;
+        } else {
+            l = mid;
+        }
+    }
+    return l;
+};
