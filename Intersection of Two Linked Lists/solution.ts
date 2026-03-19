be2b9f3f5102dceb80a3aab05632897b0c044879
Snehal Mastud <smastud0@gmail.com>
2026-03-19 13:52:19 +0530

    Create solution.ts

---
+++ Intersection of Two Linked Lists/solution.ts
@@ -0,0 +1,9 @@
+function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
+    let a = headA;
+    let b = headB;
+    while (a != b) {
+        a = a ? a.next : headB;
+        b = b ? b.next : headA;
+    }
+    return a;
+}
