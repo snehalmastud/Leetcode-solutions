35475fabf31cd512eedb0a10b875d2817f183f7e
Snehal Mastud <smastud0@gmail.com>
2026-03-19 18:24:40 +0530

    Create solution.js

---
+++ Nth Digit/solution.js
@@ -0,0 +1,12 @@
+var findNthDigit = function (n) {
+    let k = 1,
+        cnt = 9;
+    while (k * cnt < n) {
+        n -= k * cnt;
+        ++k;
+        cnt *= 10;
+    }
+    const num = Math.pow(10, k - 1) + (n - 1) / k;
+    const idx = (n - 1) % k;
+    return num.toString()[idx];
+};
