04fdf3a33eb1ebe4cf255a254fa0beebaeab6f34
Snehal Mastud <smastud0@gmail.com>
2026-03-19 11:59:44 +0530

    Create solution.ts

---
+++ Binary Tree Inorder Traversal/solution.ts
@@ -0,0 +1,6 @@
+function inorderTraversal(root: TreeNode | null): number[] {
+    if (root == null) {
+        return [];
+    }
+    return [...inorderTraversal(root.left), root.val, ...inorderTraversal(root.right)];
+}
