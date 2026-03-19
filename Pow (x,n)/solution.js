076ba71285e38a1ffd8a325cedb964639523a44b
Snehal Mastud <smastud0@gmail.com>
2026-03-19 11:29:30 +0530

    Create solution.js

---
+++ Pow (x,n)/solution.js
@@ -0,0 +1,13 @@
+var myPow = function (x, n) {
+    const qpow = (a, n) => {
+        let ans = 1;
+        for (; n; n >>>= 1) {
+            if (n & 1) {
+                ans *= a;
+            }
+            a *= a;
+        }
+        return ans;
+    };
+    return n >= 0 ? qpow(x, n) : 1 / qpow(x, -n);
+};
