60204b927deb3e2761aa3aebbfc067bdb18b73ac
Snehal Mastud <smastud0@gmail.com>
2026-03-19 15:34:44 +0530

    Create solution.py

---
+++ Palindrome Permutation II/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def generatePalindromes(self, s: str) -> List[str]:
+        def dfs(t):
+            if len(t) == len(s):
+                ans.append(t) # not using t.copy()
+                return
+            for c, v in cnt.items():
+                if v > 1:
+                    cnt[c] -= 2
+                    dfs(c + t + c)
+                    cnt[c] += 2
+
+        cnt = Counter(s)
+        mid = ''
+        for c, v in cnt.items():
+            if v & 1:
+                if mid:
+                    return []
+                mid = c
+                cnt[c] -= 1 # don't forget this
+                # actually, above is cheking 'if v > 1:', so good without this line
+        ans = []
+        dfs(mid)
+        return ans
