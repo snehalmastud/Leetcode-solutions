7992d7cda0b51d57685eeda71ccaab3a59e45441
Snehal Mastud <smastud0@gmail.com>
2026-03-21 13:51:20 +0530

    Create solution.cpp

---
+++ Convert BST to Greater Tree/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    int sum = 0;
+public:
+    TreeNode* convertBST(TreeNode* root) {
+        if (!root) return nullptr;
+        convertBST(root->right);
+        root->val = (sum += root->val);
+        convertBST(root->left);
+        return root;
+    }
+};
