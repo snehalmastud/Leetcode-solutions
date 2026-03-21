ef5ffae3319bf454496b5b85910d75f6bd7944dd
Snehal Mastud <smastud0@gmail.com>
2026-03-21 13:55:04 +0530

    Create solution.ts

---
+++ Diameter of Binary Tree/solution.ts
@@ -0,0 +1,15 @@
+function diameterOfBinaryTree(root: TreeNode | null): number {
+    let res = 0;
+    const dfs = (root: TreeNode | null) => {
+        if (root == null) {
+            return 0;
+        }
+        const { left, right } = root;
+        const l = dfs(left);
+        const r = dfs(right);
+        res = Math.max(res, l + r);
+        return Math.max(l, r) + 1;
+    };
+    dfs(root);
+    return res;
+}
