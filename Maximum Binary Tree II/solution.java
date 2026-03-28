4a2429591878875a97d5462e6d9167990c1754d7
Snehal Mastud <smastud0@gmail.com>
2026-03-28 21:21:27 +0530

    Create solution.java

---
+++ Maximum Binary Tree II/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        if (root == null || root.val < val) {
+            return new TreeNode(val, root, null);
+        }
+        root.right = insertIntoMaxTree(root.right, val);
+        return root;
+    }
+}
