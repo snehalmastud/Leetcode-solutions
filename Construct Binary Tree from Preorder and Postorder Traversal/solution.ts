189452857c91c9b05f8c83af3dd6153da8dc7199
Snehal Mastud <smastud0@gmail.com>
2026-03-28 16:35:11 +0530

    Create solution.ts

---
+++ Construct Binary Tree from Preorder and Postorder Traversal/solution.ts
@@ -0,0 +1,22 @@
+function constructFromPrePost(preorder: number[], postorder: number[]): TreeNode | null {
+    const pos: Map<number, number> = new Map();
+    const n = postorder.length;
+    for (let i = 0; i < n; ++i) {
+        pos.set(postorder[i], i);
+    }
+    const dfs = (a: number, b: number, c: number, d: number): TreeNode | null => {
+        if (a > b) {
+            return null;
+        }
+        const root = new TreeNode(preorder[a]);
+        if (a === b) {
+            return root;
+        }
+        const i = pos.get(preorder[a + 1])!;
+        const m = i - c + 1;
+        root.left = dfs(a + 1, a + m, c, i);
+        root.right = dfs(a + m + 1, b, i + 1, d - 1);
+        return root;
+    };
+    return dfs(0, n - 1, 0, n - 1);
+}
