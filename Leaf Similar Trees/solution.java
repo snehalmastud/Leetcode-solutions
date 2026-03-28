32b5d19850ffb4292c0da5c2248a4b7fde90d8d4
Snehal Mastud <smastud0@gmail.com>
2026-03-28 16:25:09 +0530

    Create solution.java

---
+++ Leaf Similar Trees/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> l1 = dfs(root1);
+        List<Integer> l2 = dfs(root2);
+        return l1.equals(l2);
+    }
+
+    private List<Integer> dfs(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> ans = dfs(root.left);
+        ans.addAll(dfs(root.right));
+        if (ans.isEmpty()) {
+            ans.add(root.val);
+        }
+        return ans;
+    }
+}
