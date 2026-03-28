7fe0d37c8f7371124d912d1ef89e83360d9cb468
Snehal Mastud <smastud0@gmail.com>
2026-03-28 17:51:12 +0530

    Create solution.java

---
+++ Univalued Binary Tree/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+        return dfs(root, root.val);
+    }
+
+    private boolean dfs(TreeNode root, int val) {
+        if (root == null) {
+            return true;
+        }
+        return root.val == val && dfs(root.left, val) && dfs(root.right, val);
+    }
+}
